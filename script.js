66e66ce4811031b5934f2d251943db18b5e43cf0
bbishay <50244538+bbishay@users.noreply.github.com>
2019-10-23 11:42:46 -0400

    forgot to clean up the code after finishing

--- script.js
+++ script.js
@@ -8,36 +8,16 @@ class Meme{
 }
 
 //take argument
-
-
-
-
 //**view */ class for the UI which contains methods
-/* class UI{
-    constructor(htmlConstructor){
-        this.htmlConstructor = () => {
-            const newMeme = new Meme(document.querySelector('.top-text'), document.querySelector('.bottom-text'), document.querySelector('.meme-img'));
-            let html = '<div class="display-image">%image%</div> <div class="display-top-text">%toptext%</div> <div class="display-bottom-text">%bottomtext%</div>'
-            let newHtml = html.replace('%toptext%', newMeme.topText);
-            newHtml = newHtml.replace('%bottomtext%', newMeme.bottomText);
-            newHtml = newHtml.replace('%image%', newMeme.image);
-            display.insertAdjacentHTML('beforeend', newHtml);
-            
-            console.log('yeet');
-        }
-    }
-} */
-
 class UI {
+//creates/insert the html for the UI DISPLAY
     htmlConstructor(newMeme){
         let html ='<div class="display-meme"><div class="display-image"><img src="%image%"></img></div> <div class="display-top-text">%toptext%</div> <div class="display-bottom-text">%bottomtext%</div><div class="remove-meme">X</div>';
         let newHtml = html.replace('%toptext%', newMeme.topText);
             newHtml = newHtml.replace('%bottomtext%', newMeme.bottomText);
             newHtml = newHtml.replace('%image%', newMeme.image);
             document.querySelector('.display').insertAdjacentHTML('beforeend', newHtml);
-            
     }
-//creates/insert the html for the UI DISPLAY
 
 //clear fields method
     clearFields(){
@@ -51,45 +31,29 @@ class UI {
     }
 }
 
-
 //controller combines UI and model
 //event handler w function
 
-
 //get values
-
-
-//instantiate new class/object album with variable name
-
-//instantiate new UI --- const ui = new UI();
-/* function eventListeners(){
-    const ui = new UI();
-    const form = document.querySelector('#meme-form');
-    form.addEventListener('submit', ui.htmlConstructor);
-}
-eventListeners(); */
-
 document.getElementById('meme-form').addEventListener('submit', function(e){
     const topText = document.getElementById('top-text').value;
     const bottomText = document.getElementById('bottom-text').value;
     const image = document.getElementById('meme-img').value;
-    
 
     if(topText.length == 0 || bottomText.length == 0 || image.length == 0){
         alert('Do not leave any areas empty');
         console.log('yeet2');
-        
     } else{
+        //instantiate new class/object album with variable name
         const newMeme = new Meme(topText, bottomText, image);
+        //instantiate new UI --- const ui = new UI();
         const ui = new UI();
+        //call relevant methods
         ui.htmlConstructor(newMeme);
         ui.clearFields();
         e.preventDefault();
     }
 });
-//call relevant methods
-
-
 
 //seperate event handler for removing
 document.querySelector('.display').addEventListener('click', function(e){
